06e11fef683dd9c9e629602e4142056628a4b385
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-06-20 11:24:00 +0200

    update

--- runLenEconding.py
+++ runLenEconding.py
@@ -1,14 +1,14 @@
 # Encoded Data
 def encodeString(stringVal):
     encodedData =[]
-    prevChar =stringVal[0]
+    prevChar =stringVal[0] # defining previous character
     count = 0
     for char in stringVal:
-        if prevChar != char:
-            encodedData.append((prevChar,count))
-            count =0
+        if prevChar != char: # comparing previous character to current character
+            encodedData.append((prevChar,count)) # add previous character to the count
+            count = 0 # reset the count 
             prevChar=char
-            count = count +1
+            count = count + 1
     encodedData.append((prevChar,char))  
     return encodedData
         
@@ -19,4 +19,5 @@ def decodeString(encodedData):
      decodedData = decodedData +item[0] *item[1]
     return decodedData 
 
-print(encodeString('AAAAABBBBCCC'))              
+print(encodeString('AAAAABBBBCCC')) 
+            
